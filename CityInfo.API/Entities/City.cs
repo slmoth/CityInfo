22321caf9e0abe156e1016fcd9de684d192f1f21
slmoth <sdmatthews01@gmail.com>
2018-08-24 16:35:16 -0400

    Added City Entity

---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Entities
+{
+    public class City
+    {
+        public int Id { get; set; }  
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<PointOfInterest> PointsOfInterest { get; set; }
+            = new List<PointOfInterest>();
+
+    }
+}
